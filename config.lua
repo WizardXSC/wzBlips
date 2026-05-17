558db25a7615357f3af891b45fdc9b5e02b3e9d9
WizardXSC <billantwn9002@gmail.com>
2026-05-17 23:00:31 +0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,5 +3,5 @@ Config = {}
 Config.EnableCustomFont = true
 
 Config.Blips = {
-    { title = "Παραδειγμα",  }
+    { title = "Παραδειγμα", colour = 1, id = 1, scale = 1.0, coords = vector3(0, 0, 0), shortRange = true }
 }
