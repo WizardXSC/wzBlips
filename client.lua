b390948f2de26b48cfa3cbf2331d98fcb645dee0
WizardXSC <billantwn9002@gmail.com>
2026-05-17 22:59:10 +0300

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,14 +1,43 @@
 local blips = {}
 
 InsertBlip = function(title, colour, id, scale, coords, shortRange)
+    local c = coords
+    if not c then
+        return
+    end
+
+    title = tostring(title)
+    colour = tonumber(colour)
+    id = tonumber(id)
+    scale = tonumber(scale)
+    shortRange = shortRange
+
+    local blip = AddBlipForCoord(c.x, c.y, c.z)
+    SetBlipSprite(blip, id)
+    SetBlipColour(blip, colour)
+    SetBlipDisplay(blip, 4)
+    SetBlipScale(blip, scale)
+    SetBlipAsShortRange(blip, shortRange)
+
+    BeginTextCommandSetBlipName("STRING")
+    if Config.EnableCustomFont then
+        AddTextComponentString('<font face="Fire Sans">' .. title .. '</font>')
+    else
+        AddTextComponentString(title)
+    end
+    EndTextCommandSetBlipName(blip)
+
     table.insert(blips, {
-        title = tostring(title),
-        colour = tonumber(colour),
-        id = tonumber(id),
-        scale = tonumber(scale),
+        title = title,
+        colour = colour,
+        id = id,
+        scale = scale,
         coords = coords,
-        shortRange = shortRange
+        shortRange = shortRange,
+        blip = blip
     })
+
+    return blip
 end
 
 CreateThread(function()
@@ -19,27 +48,10 @@ CreateThread(function()
     for k, v in pairs(Config.Blips) do
         InsertBlip(v.title, v.colour, v.id, v.scale, v.coords, v.shortRange)
     end
-    for k, v in pairs(blips) do
-        local c = v.coords
-        local blip = AddBlipForCoord(c.x, c.y, c.z)
-        SetBlipSprite(blip, v.id)
-        SetBlipColour(blip, v.colour)
-        SetBlipDisplay(blip, 4)
-        SetBlipScale(blip, v.scale)
-        SetBlipAsShortRange(blip, v.shortRange)
-        BeginTextCommandSetBlipName("STRING")
-        if Config.EnableCustomFont then
-            AddTextComponentString('<font face="Fire Sans">' .. v.title .. '</font>')
-        else
-            AddTextComponentString(v.title)
-        end
-        EndTextCommandSetBlipName(blip)
-        v.blip = blip
-    end
 end)
 
 exports('CreateBlip', function(title, colour, id, scale, coords, shortRange)
-    InsertBlip(title, colour, id, scale, coords, shortRange)
+    return InsertBlip(title, colour, id, scale, coords, shortRange)
 end)
 
 exports('GetBlips', function()
